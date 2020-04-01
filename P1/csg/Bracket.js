33f711ae6a1099e67724e9968d75bdac3bcd11f8
Vladislav Nikolov <vladis890@gmail.com>
2020-04-01 23:58:49 +0200

    Added cup

---
+++ P1/csg/Bracket.js
@@ -0,0 +1,9 @@
+/**
+ * Clase que representa una escuadra
+ */
+class Bracket extends THREE.Object3D {
+    constructor() {
+        // Llamar al constructor de la superclase
+        super();
+    }
+}
--- P1/csg/Cup.js
+++ P1/csg/Cup.js
@@ -5,6 +5,37 @@ class Cup extends THREE.Object3D {
         super();
 
         // Crear material que asigna color segun el vector normal
+        var material = this.createMaterial();
+
+        // Crear geometrias
+        var cylinder1 = new THREE.CylinderGeometry(7, 7, 15, 40);
+        var cylinder2 = new THREE.CylinderGeometry(6.5, 6.5, 14.5, 40);
+        var torus = new THREE.TorusGeometry(5, 1.5, 16, 100);
+
+        // Colocar correctamente las geometrías
+        cylinder2.translate(0, 0.25, 0);
+        torus.translate(7, 0, 0);
+
+        var cylinder1BSP = new ThreeBSP(cylinder1);
+        var cylinder2BSP = new ThreeBSP(cylinder2);
+        var torusBSP = new ThreeBSP(torus);
+
+        var partial = cylinder1BSP.union(torusBSP); // Union cilindro exterior con toro
+        var result = partial.subtract(cylinder2BSP); // Restar al resultado el cilindro interior
+
+        // Obtener mesh y normales (primero de caras y luego de vertices)
+        var mesh = result.toMesh(material);
+        mesh.geometry.computeFaceNormals();
+        mesh.geometry.computeVertexNormals();
+
+        // Crear nodo de transformaciones
+        this.translateRotate = new THREE.Object3D();
+
+        this.translateRotate.add(mesh);
+        this.add(this.translateRotate);
+    }
+
+    createMaterial() {
         var material = new THREE.MeshNormalMaterial();
 
         // Establecer que se va a usar sombreado plano y que se tiene que actualizar
@@ -12,31 +43,14 @@ class Cup extends THREE.Object3D {
         material.flatShading = true;
         material.needsUpdate = true;
 
-        // Crear geometrias
-        var sphere1 = new THREE.SphereGeometry(1.0, 10, 10);
-        var sphere2 = new THREE.SphereGeometry(1.0, 10, 10);
-
-        // Trasladar geometria de la esfera 2
-        sphere2.translate(1.0, 0.0, 0.0);
-
-        // Crear versiones de la geometria en ThreeBSP
-        var sphere1bsp = new ThreeBSP(sphere1);
-        var sphere2bsp = new ThreeBSP(sphere2);
-
-        // Sustraer la segunda esfera a la primera
-        var result = sphere1bsp.subtract(sphere2bsp);
-
-        // Obtener mesh y normales
-        var mesh = result.toMesh(material);
-        mesh.geometry.computeFaceNormals();
-        mesh.geometry.computeVertexNormals();
-
-        this.add(mesh);        
+        return material;
     }
 
     update() {
-        this.position.set(0, 0, 0);
-        this.rotation.set(0.0, 0.0, 0.0);
-        this.scale.set(1, 1, 1);
+        var rotationInc = 0.01;
+
+        this.translateRotate.rotation.y += rotationInc;
+        this.translateRotate.rotation.x += rotationInc;
+        this.translateRotate.position.set(-20, 7.5, 0);
     }
 }
